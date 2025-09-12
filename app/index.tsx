ca1c2809db9be173cc939421dee888a6d84c2ed5
Malitha <malithanew@gmail.com>
2025-09-12 09:43:11 +0530

    added header to the home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,33 @@
 import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-between flex-row w-full my-5 px-5">
+        <View className="flex-start">
+          <Text className="small-bold text-primary">DELIVER TO</Text>
+          <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+            <Text className="paragraph-bold text-dark-100">Sri Lanka</Text>
+            <Image
+              source={images.arrowDown}
+              className="size-3"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <FlatList
         data={offers}
         keyExtractor={(item) => item.id.toString()} // good practise for flatList
